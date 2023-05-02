a15224ac02f07090dd025a602452aa9ba7416897
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2023-05-02 09:49:50 +0600

    card design

---
+++ src/pages/Card/Card1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card1.css'
+const Card1 = () => {
+    return (
+        <div className='cardh-ml'>
+            <div className='card1img-ml'>
+                    <h1>this is now</h1>
+                 </div>
+             <div className='card1-ml'>
+               
+             </div>
+             
+        </div>
+    );
+};
+
+export default Card1;
